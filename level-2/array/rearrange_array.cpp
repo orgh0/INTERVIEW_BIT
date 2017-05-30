737b58c5e60a28336d3b99ba785447dbd74106a9
arghyatiger <arghyatiger@gmail.com>
2017-05-30 16:59:49 +0530

    rearrange array code added

---
+++ level-2/array/rearrange_array.cpp
@@ -0,0 +1,11 @@
+void Solution::arrange(vector<int> &A) {
+        for(int i=0; i<A.size();i++)
+        {
+        A[i] +=  ( A[A[i]] % A.size() )*A.size();
+        }
+    
+        for(int i=0;i<A.size();i++)
+        {
+        A[i] /=A.size() ;
+        }
+}
